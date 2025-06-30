f4ec01a74c0df09751371d19ae23e2b019b8e4f5
Benjamin Fauchald <benjamin@connectica.no>
2025-07-01 00:49:21 +0700

    Security fixes and CI/CD improvements

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
   include Pagy::Frontend
-  
+
   def safe_external_link(url, text = nil, options = {})
     return "" if url.blank?
-    
+
     # Ensure URL starts with http:// or https://
     safe_url = url.match?(/\Ahttps?:\/\//i) ? url : "https://#{url}"
-    
+
     # Validate URL format
     begin
       uri = URI.parse(safe_url)
@@ -14,7 +14,7 @@ module ApplicationHelper
     rescue URI::InvalidURIError
       return ""
     end
-    
+
     link_to(text || safe_url, safe_url, options.merge(target: "_blank", rel: "noopener"))
   end
 end
